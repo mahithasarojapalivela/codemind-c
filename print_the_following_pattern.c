10058ce8db8bfcb26f69a7796613f0435d4bc662
mahithasarojapalivela <119840745+mahithasarojapalivela@users.noreply.github.com>
2023-01-03 19:00:25 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=i;j++)         {             printf("%d",j);
            }
            printf("
    ");
            }
        
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
-{ 
+{
     int n;
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     { 
-        for(int j=1;j<=n;j++) 
+        for(int j=1;j<=i;j++)
         {
-            printf("%c ",i+64);
-            }
-            printf("
-"); 
-        
-    }
+            printf("%d",j); 
+            
+        }
+        printf("
+");
+        }
     
 }
