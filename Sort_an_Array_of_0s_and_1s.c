fbee107acbc76305490b57e5d7a5baae750c432b
mahithasarojapalivela <119840745+mahithasarojapalivela@users.noreply.github.com>
2023-05-08 11:45:32 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]==0)         printf("0 ");
        }
        for(i=0;i<n;i++)
        { 
            if(arr[i]==1) 
            printf("1 "); 
            
        }
        }

--- Sort_an_Array_of_0s_and_1s.c
+++ Sort_an_Array_of_0s_and_1s.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 int main()
-{ 
+{
     int n,i;
     scanf("%d",&n);
-    int arr[n]; 
-    for(i=0;i<n;i++) 
-    {
+    int arr[n];
+    for(i=0;i<n;i++)
+    { 
         scanf("%d",&arr[i]); 
-        if(arr[i]==0)
-        printf("0 ");
-        }
-        for(i=0;i<n;i++)
-        {
-            if(arr[i]==1)
-            printf("1 ");
-            }
-            }
+        if(arr[i]==0) 
+        printf("0 "); 
+        
+    }
+    for(i=0;i<n;i++)
+    { 
+        if(arr[i]==1) 
+        printf("1 "); 
+        
+    }
+    }
