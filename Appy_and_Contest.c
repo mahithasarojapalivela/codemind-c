ec75accb43ae24c7a7039a4ee363bfff32cc1f09
mahithasarojapalivela <119840745+mahithasarojapalivela@users.noreply.github.com>
2023-05-19 11:24:27 +0530

    #include <stdio.h> int main(void) {     int t;     long long n,a,b,k,count;     scanf("%d",&t);     while(t--)     {         scanf("%lld%lld%lld%lld",&n,&a,&b,&k);         if(a%b==0)         count=n/b-n/a;         else if(b%a==0)         count=n/a-n/b;         else count=n/a+n/b-2*(n/(a*b));         if(count>=k)         printf("Win ");         else printf("Lose ");     }     return 0; }

--- Appy_and_Contest.c
+++ Appy_and_Contest.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-int main(void) 
+int main(void)
 {
     int t; 
-    long long n,a,b,k,count;
+    long long n,a,b,k,count; 
     scanf("%d",&t); 
-    while(t--) 
+    while(t--)
     {
-        scanf("%lld%lld%lld%lld",&n,&a,&b,&k); 
-        if(a%b==0) 
-        count=n/b-n/a;
-        else
-        if(b%a==0)
-        count=n/a-n/b; 
-        else
-        count=n/a+n/b-2*(n/(a*b)); 
+        scanf("%lld%lld%lld%lld",&n,&a,&b,&k);
+        if(a%b==0)
+        count=n/b-n/a; 
+        else if(b%a==0)
+        count=n/a-n/b;
+        else count=n/a+n/b-2*(n/(a*b)); 
         if(count>=k)
         printf("Win
-"); 
-        else
-        printf("Lose
 ");
-        }
-        return 0; 
-    
+        else printf("Lose
+"); 
+    }
+    return 0;
 }
